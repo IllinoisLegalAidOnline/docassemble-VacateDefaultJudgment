4c0cafe73911a7510f78f061d3489889c22d93c4
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-01-09 17:48:47 -0600

    interview and form tweaks before final dev testing

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.VacateDefaultJudgmentWithin30Days',
-      version='0.1.1',
+      version='0.1.2',
       description=('A docassemble extension.'),
       long_description='# docassemble.Motion\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -52,9 +52,8 @@ setup(name='docassemble.VacateDefaultJudgmentWithin30Days',
       author_email='mnewsted@illinoislegalaid.org',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=3.1.0', 'docassemble.ILAO>=1.0.99'],
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.ILAO>=1.0.99'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VacateDefaultJudgmentWithin30Days/', package='docassemble.VacateDefaultJudgmentWithin30Days'),
      )
