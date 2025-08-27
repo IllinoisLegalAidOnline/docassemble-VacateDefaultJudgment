a956a023df3de7e4cfde63915acc434ef9414394
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-08-27 09:13:14 -0500

    name_full updates plus interview standards alignment

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.VacateDefaultJudgment',
-      version='1.0.1',
+      version='1.0.2',
       description=('A docassemble extension.'),
       long_description='# docassemble.Motion\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -53,7 +53,7 @@ setup(name='docassemble.VacateDefaultJudgment',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble.ILAO>=1.0.99'],
+      install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VacateDefaultJudgment/', package='docassemble.VacateDefaultJudgment'),
      )
